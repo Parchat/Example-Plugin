77f5eb3086bad405de0c84731e2294cb29c9af7c
Ryder Belserion <github@corecraft.me>
2022-07-10 00:11:16 -0400

    Add chat debug

--- src/main/java/me/example/exampleplugin/events/ExampleEvent.java
+++ src/main/java/me/example/exampleplugin/events/ExampleEvent.java
@@ -1,14 +1,19 @@
 package me.example.exampleplugin.events;
 
 import io.papermc.paper.event.player.AsyncChatEvent;
+import me.example.exampleplugin.api.ExampleManager;
 import net.kyori.adventure.text.Component;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 
 public class ExampleEvent implements Listener {
 
+    private final ExampleManager exampleManager = new ExampleManager();
+
     @EventHandler
     public void onAsyncChatEvent(AsyncChatEvent event) {
+        exampleManager.getPlugin().getLogger().info(event.getPlayer().getName());
+
         event.getPlayer().sendMessage(Component.text("Guten Tag!"));
     }
 }
